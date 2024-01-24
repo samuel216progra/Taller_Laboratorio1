342dcf42936cb648d52982558fd3f1742fdf150d
samuel216progra <srodriguez-2022165@kinal.edu.gt>
2024-01-24 16:04:06 -0600

    Se agrego card

---
+++ asses/Logica.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var nav = document.getElementById("myNav");
+    nav.style.display = (nav.style.display === "block") ? "none" : "block";
+}
+function flipCard() {
+    var card = document.querySelector('.card');
+    card.style.transform = card.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+}
