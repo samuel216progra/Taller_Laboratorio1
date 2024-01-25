6835905c00e471f9e2e40a6a6427ccc9c93b34ab
samuer216progra <srodriguez-2022165@kinal.edu.gt>
2024-01-25 08:58:18 -0600

    Se termino Pagina Principal

--- asses/Logica.js
+++ asses/Logica.js
@@ -6,3 +6,9 @@ function flipCard() {
     var card = document.querySelector('.card');
     card.style.transform = card.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
 }
+
+
+
+function expandCard(card) {
+    card.classList.toggle('expanded');
+}
